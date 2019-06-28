aaec9871908e809fa4f9834574d42a9f6761ede8
WhomHim <534801304@qq.com>
2019-06-28 17:42:44 +0800

    refactor:整理代码

--- src/main/java/com/exersise/serilizable.java
+++
@@ -1,4 +0,0 @@
-package com.exersise;
-
-public class serilizable {
-}
--- src/main/java/com/simgletonpattern/Minister.java
+++ src/main/java/com/simgletonpattern/Minister.java
@@ -1,4 +1,4 @@
-package com.simgleton_pattern;
+package com.simgletonpattern;
 
 public class Minister {
     public static void main(String[] args) {
