36c155c454b38a22b76ee1d19024569e2089fd25
WhomHim <534801304@qq.com>
2019-12-18 11:28:15 +0800

    feat:新增Predicate示例

---
+++ src/main/java/com/lambda/PredicateTest.java
@@ -0,0 +1,45 @@
+package com.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * @author WhomHim
+ * @description 函数式接口Predicate
+ * @date Create in 2019/12/12 19:15
+ */
+@SuppressWarnings("unchecked")
+public class PredicateTest {
+    public static void main(String[] args) {
+        List languages = Arrays.asList("Java", "Scala", "C++", "Haskell", "Lisp");
+
+        System.out.println("Print all languages :");
+        filter(languages, (str) -> true);
+
+        System.out.println("Print no language : ");
+        filter(languages, (str) -> false);
+
+        /* java.util.function.Predicate 允许将两个或更多的 Predicate 合成一个。它提供类似于逻辑操作符AND和OR的方法，
+        名字叫做and()、or()和xor()，用于将传入 filter()           方法的条件合并起来。例如，要得到所有以J开始，长度为
+        四个字母的语言，可以定义两个独立的 Predicate 示例分别表示每一个条件，然后用 Predicate.and()
+        方法将它们合并起来，如下所示： */
+        /*  甚至可以用and()、or()和xor()逻辑函数来合并Predicate，
+          例如要找到所有以J开始，长度为四个字母的名字，你可以合并两个Predicate并传入*/
+        Predicate<String> startsWithJ = (n) -> n.startsWith("J");
+        Predicate<String> fourLetterLong = (n) -> n.length() == 4;
+
+        languages.stream()
+                .filter(startsWithJ.and(fourLetterLong))
+                .forEach((n) -> System.out.print("nName, which starts with 'J' and four letter long is : " + n));
+
+    }
+
+    private static void filter(List names, Predicate condition) {
+        for (Object name : names) {
+            if (condition.test(name)) {
+                System.out.println(name + " ");
+            }
+        }
+    }
+}
