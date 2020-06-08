151bc8f97386ca06313e551aa6d4fc2f2f372c05
whomhim <534801304@qq.com>
2020-06-09 00:15:39 +0800

    feat:增加链表

---
+++ src/main/java/com/algorithm/ListNode.java
@@ -0,0 +1,19 @@
+package com.algorithm;/*
+
+/**
+ * @author WhomHim
+ * @description
+ * @date Create in 2020-6-8 15:34:59
+ */
+
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+
+
