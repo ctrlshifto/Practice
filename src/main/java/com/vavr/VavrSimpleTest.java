4d0055d2e9a877b30456ddc69bacb40c6fbb70bb
whohim <huqian@deepexi.com>
2020-09-09 10:13:05 +0800

    feat: 新增 vavr stream demo

--- src/main/java/com/vavr/VavrSimpleTest.java
+++ src/main/java/com/vavr/VavrSimpleTest.java
@@ -3,6 +3,8 @@ package com.vavr;
 import io.vavr.Tuple;
 import io.vavr.*;
 import io.vavr.collection.List;
+import io.vavr.collection.Map;
+import io.vavr.collection.Stream;
 import io.vavr.control.Either;
 import io.vavr.control.Try;
 import org.junit.Test;
@@ -166,6 +168,33 @@ public class VavrSimpleTest {
                 }))
         );
         System.out.println(of);
+    }
+
+    @Test
+    public void stream() {
+        Map<Boolean, List<Integer>> tuple2s = Stream.ofAll(1, 2, 3, 4, 5)
+                .groupBy(v -> v % 2 == 0)
+                .mapValues(Value::toList);
+        System.out.println(tuple2s);
+        // 输出 LinkedHashMap((false, List(1, 3, 5)), (true, List(2, 4)))
+
+        Tuple2<List<Integer>, List<Integer>> listTuple2 = Stream.ofAll(1, 2, 3, 4)
+                .partition(v -> v > 2)
+                .map(Value::toList, Value::toList);
+        System.out.println(listTuple2);
+        // 输出 (List(3, 4), List(1, 2))
+
+        List<Integer> integers = Stream.ofAll(List.of("Hello", "World", "a"))
+                .scanLeft(0, (sum, str) -> sum + str.length())
+                .toList();
+        System.out.println(integers);
+        // 输出 List(0, 5, 10, 11)
+
+        List<Tuple2<Integer, String>> tuple2List = Stream.ofAll(1, 2, 3)
+                .zip(List.of("a", "b"))
+                .toList();
+        System.out.println(tuple2List);
+        // 输出 List((1, a), (2, b))
     }
 
     private void hello() {
