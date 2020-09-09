b147dc9d22baf517ee4be6e424bfe097f6e555d3
whohim <huqian@deepexi.com>
2020-09-09 11:03:24 +0800

    feat: 新增 vavr partialApplication/currying demo

--- src/main/java/com/vavr/VavrSimpleTest.java
+++ src/main/java/com/vavr/VavrSimpleTest.java
@@ -71,9 +71,29 @@ public class VavrSimpleTest {
         System.out.println(either.getLeft());
     }
 
-    private static void functions() {
+    @Test
+    public void partialApplication() {
+        //sum 函数通过部分应用，第一个参数被固定为 2，并产生新的 add2 函数。
         Function2<Integer, Integer, Integer> sum = Integer::sum;
-        System.out.println(sum);
+        Function1<Integer, Integer> add2 = sum.apply(2);
+        System.out.println(add2.apply(1));
+    }
+
+    /**
+     * 柯里化是把接受多个参数的函数变换成接受一个单一参数（最初函数的第一个参数）的函数，
+     * 并且返回接受余下的参数而且返回结果的新函数的技术。
+     */
+    @Test
+    public void currying() {
+        Function3<Integer, Integer, Integer, Integer> sum = (a, b, c) -> a + b + c;
+        final Function1<Integer, Function1<Integer, Integer>> add2 = sum.curried().apply(2);
+
+        // = 9
+        Integer i = add2.apply(4).apply(3);
+        System.out.println(i);
+    }
+
+    private static void functions() {
         //使用 andThen
         Function1<Integer, Integer> plusOne = a -> a + 1;
         Function1<Integer, Integer> multiplyByTwo = a -> a * 2;
