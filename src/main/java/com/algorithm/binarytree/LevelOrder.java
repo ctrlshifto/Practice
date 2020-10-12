5505539d8c4bca4c13308704f37fdd05b0402079
whomhim <534801304@qq.com>
2020-10-12 22:35:46 +0800

    chore: change code style

--- src/main/java/com/algorithm/binarytree/LevelOrder.java
+++ src/main/java/com/algorithm/binarytree/LevelOrder.java
@@ -8,24 +8,24 @@ import java.util.List;
  * @author WhomHim
  * @description 二叉树的层序遍历
  * <p>
- *     给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
- *
+ * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+ * <p>
  * 示例：
  * 二叉树：[3,9,20,null,null,15,7],
- *
- *     3
- *    / \
- *   9  20
- *     /  \
- *    15   7
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
  * 返回其层次遍历结果：
- *
+ * <p>
  * [
- *   [3],
- *   [9,20],
- *   [15,7]
+ * [3],
+ * [9,20],
+ * [15,7]
  * ]
- *
+ * <p>
  * 作者：力扣 (LeetCode)
  * 链接：https://leetcode-cn.com/leetbook/read/data-structure-binary-tree/xefh1i/
  *
@@ -33,34 +33,34 @@ import java.util.List;
  * @date Create in 2020-10-9 22:52:30
  */
 public class LevelOrder {
-    private static List<List<Integer>> levelOrder(TreeNode root){
+    private static List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> returnList = new ArrayList<>();
-        if (root==null){
+        if (root == null) {
             return returnList;
         }
         LinkedList<TreeNode> queue = new LinkedList<>();
         queue.push(root);
 
-        while ( queue.size()>0){
+        while (!queue.isEmpty()) {
             //获取当前队列的长度，这个长度相当于 当前这一层的节点个数
             int levelNodes = queue.size();
 
-            List<Integer>tmp = new ArrayList<>();
+            List<Integer> tmp = new ArrayList<>();
             // 不断的遍历队列，将每一层的元素放进队列
             for (int i = 0; i < levelNodes; ++i) {
                 TreeNode treeNode = queue.remove();
                 tmp.add(treeNode.val);
-                if (treeNode.left!=null){
+                if (treeNode.left != null) {
                     queue.add(treeNode.left);
                 }
-                if (treeNode.right!=null){
+                if (treeNode.right != null) {
                     queue.add(treeNode.right);
                 }
             }
             returnList.add(tmp);
         }
 
-        return  returnList;
+        return returnList;
     }
 
     public static void main(String[] args) {
