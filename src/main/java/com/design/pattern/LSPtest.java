d819720541413ce3bd9c3f6854fa249e9f8a1d57
Whomhim <534801304@qq.com>
2021-01-30 18:06:12 +0800

    feat: add 里氏替换原则

---
+++ src/main/java/com/design/pattern/LSPtest.java
@@ -0,0 +1,53 @@
+package com.design.pattern;
+
+/**
+ * @author WhomHim
+ * @description <p>http://c.biancheng.net/view/1324.html</p>
+ * <p>里氏替换原则——面向对象设计原则</p>
+ * @date Create in 2021/1/30 18:01
+ */
+public class LSPtest {
+    public static void main(String[] args) {
+        Bird bird1 = new Swallow();
+        Bird bird2 = new BrownKiwi();
+        bird1.setSpeed(120);
+        bird2.setSpeed(120);
+        System.out.println("如果飞行300公里：");
+        try {
+            System.out.println("燕子将飞行" + bird1.getFlyTime(300) + "小时.");
+            System.out.println("几维鸟将飞行" + bird2.getFlyTime(300) + "小时。");
+        } catch (Exception err) {
+            System.out.println("发生错误了!");
+        }
+    }
+}
+
+class Bird {
+    double flySpeed;
+
+    public void setSpeed(double speed) {
+        flySpeed = speed;
+    }
+
+    public double getFlyTime(double distance) {
+        return (distance / flySpeed);
+    }
+}
+
+/**
+ * 燕子类
+ */
+class Swallow extends Bird {
+
+}
+
+/**
+ * 几维鸟类
+ */
+class BrownKiwi extends Bird {
+
+    @Override
+    public void setSpeed(double speed) {
+        flySpeed = 0;
+    }
+}
