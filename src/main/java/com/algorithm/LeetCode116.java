41d5cc400feb17030c9b76e78c14fd8458ffbac6
whohim <534801304@qq.com>
2021-02-10 23:27:34 +0800

    feat: add LeetCode 116 recursive way

--- src/main/java/com/algorithm/LeetCode116.java
+++ src/main/java/com/algorithm/LeetCode116.java
@@ -49,6 +49,27 @@ public class LeetCode116 {
         return root;
     }
 
+    public static Node connectRecursive(Node root) {
+        if (root == null) {
+            return null;
+        }
+        connectTwoNode(root.left, root.right);
+        return root;
+    }
+
+    private static void connectTwoNode(Node node1, Node node2) {
+        if (node1 == null || node2 == null) {
+            return;
+        }
+        node1.next = node2;
+        // 连接相同父节点的两个子节点
+        connectTwoNode(node1.left, node1.right);
+        connectTwoNode(node2.left, node2.right);
+
+        // 连接不同父节点的两个子节点
+        connectTwoNode(node1.right, node2.left);
+    }
+
     public static void main(String[] args) {
         Node treeNode3 = new Node(3)
                 .setLeft(new Node(6))
