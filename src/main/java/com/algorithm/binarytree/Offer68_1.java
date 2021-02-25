bec2ce7da66badfd33bece4637f4bacd808e18ec
whohim <534801304@qq.com>
2021-02-25 23:36:41 +0800

    feat: add offer 68-1

--- src/main/java/com/algorithm/binarytree/Offer68_1.java
+++ src/main/java/com/algorithm/binarytree/Offer68_1.java
@@ -7,11 +7,11 @@ package com.algorithm.binarytree;
  */
 public class Offer68_1 {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(p.val<root.val&& q.val<root.val){
-            return lowestCommonAncestor(root.left,p,q);
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
         }
-        if(p.val > root.val && q.val > root.val){
-            return lowestCommonAncestor(root.right,p,q);
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
         }
         return root;
     }
